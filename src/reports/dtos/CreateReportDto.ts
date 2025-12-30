d3d38690fa1d16394366fcdbdb975da8b6c1613b
Kaiser Kamruzzaman <38045495+KaiserKamruzzaman@users.noreply.github.com>
2025-12-30 21:45:47 +0100

    feat: add relation with user

---
+++ src/reports/dtos/CreateReportDto.ts
@@ -0,0 +1,39 @@
+import {
+  IsString,
+  IsNumber,
+  Min,
+  Max,
+  IsLongitude,
+  IsLatitude,
+} from 'class-validator';
+
+export class CreateReportDto {
+  @IsString()
+  make: string;
+
+  @IsString()
+  model: string;
+
+  @IsNumber()
+  @Min(1930)
+  @Max(new Date().getFullYear() + 1)
+  year: number;
+
+  @IsNumber()
+  @IsLongitude()
+  lng: number;
+
+  @IsNumber()
+  @IsLatitude()
+  lat: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  price: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  mileage: number;
+}
